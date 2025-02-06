de199a640c3094458c2b3acae58e505f769a30f0
Tharun-hash-dev <bhukyatharun67@gmail.com>
2025-02-06 12:43:27 +0530

    Update StudentRepository.java

--- src/main/java/com/task/repository/StudentRepository.java
+++ src/main/java/com/task/repository/StudentRepository.java
@@ -1,1 +1,14 @@
 
+package com.task.repository;
+
+import com.task.entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findByNameContainingIgnoreCase(String name); 
+    Student findByUniqueStudentCode(String uniqueStudentCode);
+}
+-
