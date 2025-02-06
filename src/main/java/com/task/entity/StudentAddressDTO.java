eda902f180e5ef81081e7ba0c60d484775ce6228
Tharun-hash-dev <bhukyatharun67@gmail.com>
2025-02-06 12:40:47 +0530

    Create StudentAddressDTO.java

---
+++ src/main/java/com/task/entity/StudentAddressDTO.java
@@ -0,0 +1,42 @@
+package com.task.entity;
+
+import java.util.List;
+
+public class StudentAddressDTO {
+    private String area;
+    private String state;
+    private String district;
+    private String pincode;
+    private String addressType;
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	public String getPincode() {
+		return pincode;
+	}
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+	public String getAddressType() {
+		return addressType;
+	}
+	public void setAddressType(String addressType) {
+		this.addressType = addressType;
+	}
+}
+
