9a755d6c4d447d78ff9bc418394bae5ce9598d07
Tharun-hash-dev <bhukyatharun67@gmail.com>
2025-02-06 12:54:00 +0530

    Update GlobalExceptionHandler.java

--- src/main/java/com/task/ValidationsandConfigurations/GlobalExceptionHandler.java
+++ src/main/java/com/task/ValidationsandConfigurations/GlobalExceptionHandler.java
@@ -1,1 +1,8 @@
 
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RuntimeException.class) 
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage()); 
+    }
+}
