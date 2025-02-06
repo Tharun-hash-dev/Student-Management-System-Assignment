d4c78385f23ae8935c0b19d9b3d34092bb2cf1ee
Tharun-hash-dev <bhukyatharun67@gmail.com>
2025-02-06 12:56:41 +0530

    Update SwaggerConfiguration.java

--- src/main/java/com/task/ValidationsandConfigurations/SwaggerConfiguration.java
+++ src/main/java/com/task/ValidationsandConfigurations/SwaggerConfiguration.java
@@ -1,3 +1,5 @@
+package com.task.validationsandConfigurations;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
